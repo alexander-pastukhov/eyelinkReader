33fc6584905cb711fcc52f39b3850bc62f9d093f
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2018-05-10 17:41:41 +0200

    Added read_trial_header

--- src/read_edf_file.cpp
+++ src/read_edf_file.cpp
@@ -8,6 +8,8 @@ using namespace Rcpp;
 
 #include "edf_structures.h"
 
+//' Opens EDF file, throws exception on error
+//'
 //' @title Opens EDF file, throws exception on error
 //' @description Opens EDF file for reading, throws exception and prints error message if fails.
 //' @param std::string filename, name of the EDF file
@@ -33,6 +35,8 @@ EDFFILE* safely_open_edf_file(std::string filename, int consistency, int loadeve
   return edfFile;
 }
 
+//' Reads preamble of the EDF file.
+//'
 //' @title Reads preamble of the EDF file.
 //' @description Reads preamble of the EDF file.
 //' @return string with the preamble
@@ -89,6 +93,9 @@ void set_trial_navigation_up(EDFFILE* edfFile, std::string start_marker_string, 
   delete[] end_marker_char;
 }
 
+
+//' Jumps to the i-th trial
+//'
 //' @title Jumps to the i-th trial
 //' @description Jumps to the i-th trial, throws an exception and prints an error message, if fails.
 //' @param EDFFILE* edfFile, pointer to the EDF file
@@ -103,6 +110,8 @@ void jump_to_trial(EDFFILE* edfFile, int iTrial){
   }
 }
 
+//' Prepare matrix for trial headers
+//'
 //' @title Prepare matrix for trial headers
 //' @description Prepare matrix for trial headers.
 //' @param int total_trials, total number of trials, i.e. number of rows in the matrix
@@ -111,7 +120,7 @@ void jump_to_trial(EDFFILE* edfFile, int iTrial){
 NumericMatrix prepare_trial_headers(int total_trials){
   // row names
   NumericVector row_index(total_trials);
-  for(unsigned int iTrial= 0; iTrial< total_trials; iTrial++)
+  for(int iTrial= 0; iTrial< total_trials; iTrial++)
   {
     row_index[iTrial]= iTrial+1;
   };
@@ -128,3 +137,42 @@ NumericMatrix prepare_trial_headers(int total_trials){
   trial_headers.attr("dimnames")= List::create(row_index, col_names);
   return (trial_headers);
 }
+
+//' Read header for the i-th trial
+//'
+//' @title Read header for the i-th trial
+//' @description Read head and store it in the i-th row of the headers matrix
+//' @param EDFFILE* edfFile, pointer to the EDF file
+//' @param NumericMatrix &trial_headers, reference to the trial header matrix
+//' @param int iTrial, the row in which the header will be stored.
+//' Functions assumes that the correct trial within the EDF file was already navigated to.
+//' @return modifes trial_headers i-th row in place
+//' @keywords internal
+void read_trial_header(EDFFILE* edfFile, NumericMatrix &trial_headers, int iTrial){
+
+  // obtaining the trial header
+  TRIAL current_header;
+  if (edf_get_trial_header(edfFile, &current_header) != 0){
+    std::stringstream error_message_stream;
+    error_message_stream << "Error obtaining the header for the trial " << iTrial+1;
+    ::Rf_error(error_message_stream.str().c_str());
+  }
+
+  // copying it over
+  trial_headers(iTrial, 0)= iTrial+1;
+  trial_headers(iTrial, 1)= current_header.duration;
+  trial_headers(iTrial, 2)= current_header.starttime;
+  trial_headers(iTrial, 3)= current_header.endtime;
+  trial_headers(iTrial, 4)= current_header.rec->time;
+  trial_headers(iTrial, 5)= current_header.rec->sample_rate ;
+  trial_headers(iTrial, 6)= current_header.rec->eflags;
+  trial_headers(iTrial, 7)= current_header.rec->sflags;
+  trial_headers(iTrial, 8)= current_header.rec->state;
+  trial_headers(iTrial, 9)= current_header.rec->record_type;
+  trial_headers(iTrial,10)= current_header.rec->pupil_type;
+  trial_headers(iTrial,11)= current_header.rec->recording_mode;
+  trial_headers(iTrial,12)= current_header.rec->filter_type;
+  trial_headers(iTrial,13)= current_header.rec->pos_type;
+  trial_headers(iTrial,14)= current_header.rec->eye;
+}
+
