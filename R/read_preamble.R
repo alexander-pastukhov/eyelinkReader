b3cb2329d145468a8ae0e5c5dbe0a07b0f841f5d
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2018-05-21 21:03:16 +0200

    A wrapper for read_preamble with better looking output

---
+++ R/read_preamble.R
@@ -0,0 +1,32 @@
+#' Reads edf-file preamble
+#'
+#' @description Read the preamble of the EDF file and parses it into an reading-friendly format
+#' @param file name of the EDF file
+#'
+#' @return an object of class "edfR_preamble"
+#' @export
+#'
+#' @examples
+#'  read_preamble(system.file("extdata", "example.edf", package = "edfR"))
+read_preamble <- function(file){
+  # getting the preamble as a single string and splitting it by new-line
+  preamble <- read_preamble_str(file) %>%
+    stringr::str_split('\\n', simplify = FALSE)
+
+  # removing leading '** ', cause why would we need them?
+  preamble <- preamble[[1]] %>%
+    stringr::str_remove_all('[\\*]{2} ')
+
+  # dropping any empty strings
+  preamble <- preamble[sapply(preamble, stringr::str_length)>0]
+
+  # assigning class name
+  class(preamble) <- 'edfR_preamble'
+  preamble
+}
+
+
+#' @export
+print.edfR_preamble <- function(x, ...){
+  preamble_output <- lapply(preamble, function(x){cat(x); cat("\n")})
+}
