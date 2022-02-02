90eea77d00399dc0d10b0634474560c54822d545
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-02 20:46:08 +0100

    Function to check logical flags for validity

---
+++ R/check_logical_flag.R
@@ -0,0 +1,19 @@
+#' Checks for validity of a logical flag, stops if not valid
+#'
+#' @param logical_flag Logical scalar
+#'
+#' @return logical, value of the logical_flag
+#' @export
+#'
+#' @examples
+#' import_samples <- TRUE
+#' check_logical_flag(import_samples)
+check_logical_flag <- function(logical_flag){
+  y <<- match.call()
+  param_name <- as.character(match.call()[[2]])
+  print(param_name)
+  if (!is.logical(logical_flag)) stop(sprintf("%s must be a scalar logical value", param_name))
+  if (length(logical_flag) != 1) stop(sprintf("%s must be a scalar logical value", param_name))
+
+  logical_flag
+}
