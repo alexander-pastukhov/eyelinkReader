0aab07d16025d86ad7742d7b4d48d9367142f2ed
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-02 19:46:53 +0100

    Dropped a hacky way to suppress warning about global variables

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-# This is merely a heck to prevent "no visible binding for global variable" notes
-# I am not particularly fond of it, but it appears to be the preferred solution
-if(getRversion() >= "2.15.1"){
-  utils::globalVariables(c("type", "entime", "entime_rel", "duration", "eye", "time",
-                           "read", "status", "flags", "input", "buttons", "parsedby",
-                           "message", "assignment", "assignment2", "variable", "value", "trial", "sttime", "sttime_rel"))
-}
