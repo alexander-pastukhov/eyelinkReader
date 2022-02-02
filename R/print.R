7b0fa67cfb83e1091826b3745b7098da944b4ec3
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-02 21:02:33 +0100

    Print in a separate file

---
+++ R/print.R
@@ -0,0 +1,41 @@
+#' Print info about \code{\link{edfRecording}}
+#'
+#' @param x \code{\link{edfRecording} object
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' recording <- read_edf(system.file("extdata", "example.edf", package = "edfR"))
+#' print(recording)
+print.edfRecording <- function(x, ...){
+  if (nrow(x$headers)==1){
+    trialsN <- 'one trial'
+  }
+  else{
+    trialsN <- sprintf('%d trials', nrow(x$headers))
+  }
+
+  if ('events' %in% names(x)){
+    if ('samples' %in% names(x))
+    {
+      cat(sprintf('%d events and %d samples in %s.\n', nrow(x$events), nrow(x$samples), trialsN))
+    }
+    else{
+      cat(sprintf('%d events in %s.\n', nrow(x$events), trialsN))
+    }
+  }
+  else{
+    if ('samples' %in% names(x))
+    {
+      cat(sprintf('%d samples in %s.\n', nrow(x$samples), trialsN))
+    }
+    else{
+      cat(sprintf('%s. Neither events nor samples were imported.', trialsN))
+    }
+  }
+
+  cat('Preamble:\n')
+  print(x$preamble)
+}
