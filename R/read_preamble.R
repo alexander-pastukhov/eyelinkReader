52fdae31ae8db69f4595409b66f45cd9ceda9074
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-02 21:07:36 +0100

    Explicit imports

--- R/read_preamble.R
+++ R/read_preamble.R
@@ -3,19 +3,19 @@
 #' @description Read the preamble of the EDF file and parses it into an reading-friendly format
 #' @param file name of the EDF file
 #'
-#' @return an object of class "edfR_preamble"
+#' @return an object of class \code{\link{edfR_preamble}}
 #' @export
+#' @importFrom stringr str_split str_remove_all
 #'
 #' @examples
-#'  read_preamble(system.file("extdata", "example.edf", package = "edfR"))
+#' read_preamble(system.file("extdata", "example.edf", package = "edfR"))
 read_preamble <- function(file){
   # getting the preamble as a single string and splitting it by new-line
   preamble <- read_preamble_str(file) %>%
     stringr::str_split('\\n', simplify = FALSE)
 
   # removing leading '** ', cause why would we need them?
-  preamble <- preamble[[1]] %>%
-    stringr::str_remove_all('[\\*]{2} ')
+  preamble <- stringr::str_remove_all(preamble[[1]], '[\\*]{2} ')
 
   # dropping any empty strings
   preamble <- preamble[sapply(preamble, stringr::str_length)>0]
