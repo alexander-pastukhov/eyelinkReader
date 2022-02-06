a15fde4c86494d884a2c719bace4326e37365080
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-06 20:08:06 +0100

    Attempting to compile during onLoad

---
+++ R/zzz.R
@@ -0,0 +1,21 @@
+.onLoad <- function(libname, pkgname) {
+  variable <- 42
+  assign("variable", variable, envir = parent.env(environment()))
+  # pkg_ns_env <- parent.env(environment())
+  # temp_dir <- tempdir()
+  # file.copy(system.file("cpp", "convert_NAs.cpp", package = "eyelinkReader"), temp_dir)
+  # filename <- paste0(temp_dir, "/convert_NAs.cpp")
+  # # print(filename)
+  # compiled_code <- Rcpp::sourceCpp(filename, env = pkg_ns_env)
+  # value <- 42
+  # assign("convert_missing_to_NAs", value, envir = parent.env(environment()))
+  # assign("convert_missing_to_NAs", pkg_ns_env$convert_missing_to_NAs, envir = parent.env(environment()))
+  # eyelinkReader::convert_missing_to_NAs <- pkg_ns_env$convert_missing_to_NAs
+  # if (exists("convert_missing_to_NAs")) {
+  #   assign("convert_missing_to_NAs", convert_missing_to_NAs, pkg_ns_env)
+  #   rm("convert_missing_to_NAs")
+  # }
+  # compiled_code <- Rcpp::sourceCpp(system.file("cpp", "convert_NAs.cpp", package = "eyelinkReader"),
+                                   # env = pkg_ns_env)
+  # lapply(compiled_code$functions, function(name) assign(name, the_module[[name]], pkg_ns_env))
+}
