7b9572c3a4f1d2dea8a590159c9671ab43f93835
Alexander Pastukhov <pastukhov.alexander@gmail.com>
2022-02-07 10:16:25 +0100

    Runtime check that library was compiled

---
+++ R/check_that_compiled.R
@@ -0,0 +1,29 @@
+#' Checks whether EDF API library was present and
+#' interface was successfully be compiled
+#'
+#' @param fail_loudly logical, whether lack of compiled library means
+#' error (\code{TRUE}), just warning (\code{FALSE}), or silent (\code{NA},
+#' for test use only).
+#'
+#' @return logical
+#' @importFrom methods getPackageName
+#' @importFrom rlang env
+#' @export
+#'
+#' @examples
+#' check_that_compiled()
+check_that_compiled <- function(fail_loudly = TRUE){
+  is_compiled <- exists('read_edf_file',
+                        where=paste0('package:', getPackageName(env())),
+                        mode='function')
+
+  if (!is_compiled && !is.na(fail_loudly)) {
+    if (fail_loudly) {
+      stop("No compiled interface to EDF API. Please see diagnostic messages when package is loading.")
+    } else {
+      warning("No compiled interface to EDF API, function will return NULL. Please see diagnostic messages when package is loading.")
+    }
+  }
+
+  is_compiled
+}
