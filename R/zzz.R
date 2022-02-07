860f8ca31d990a0d2fc7b9bb3590cf25034c3d74
Alexander Pastukhov <pastukhov.alexander@gmail.com>
2022-02-07 10:17:49 +0100

    No compilation yet (need to figure out checks)

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   temp_dir <- tempdir()
-  file.copy(system.file("cpp", "convert_NAs.cpp", package = pkgname), temp_dir)
-  filename <- paste0(temp_dir, "/convert_NAs.cpp")
-    compiled_code <- Rcpp::sourceCpp(filename, env = parent.env(environment()))
+  file.copy(system.file("cpp", "edf_interface.cpp", package = pkgname), temp_dir)
+  filename <- paste0(temp_dir, "/edf_interface.cpp")
+  # compiled_code <- Rcpp::sourceCpp(filename, env = parent.env(environment()))
 }
