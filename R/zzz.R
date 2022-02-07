84391b492d7b499b6966d3c97301c45a97575b8a
Alexander Pastukhov <pastukhov.alexander@gmail.com>
2022-02-07 09:11:09 +0100

    Package name from parametersd

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   temp_dir <- tempdir()
-  file.copy(system.file("cpp", "convert_NAs.cpp", package = "eyelinkReader"), temp_dir)
+  file.copy(system.file("cpp", "convert_NAs.cpp", package = pkgname), temp_dir)
   filename <- paste0(temp_dir, "/convert_NAs.cpp")
-  compiled_code <- Rcpp::sourceCpp(filename, env = parent.env(environment()))
+    compiled_code <- Rcpp::sourceCpp(filename, env = parent.env(environment()))
 }
