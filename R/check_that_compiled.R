c27b980381eba524b3021796909a46c13c0187d4
Alexander Pastukhov <pastukhov.alexander@gmail.com>
2022-02-08 12:51:05 +0100

    Helped is_compiled function

--- R/check_that_compiled.R
+++ R/check_that_compiled.R
@@ -5,19 +5,12 @@
 #' error (\code{TRUE}), just warning (\code{FALSE}), or silent (\code{NA},
 #' for test use only).
 #'
-#' @return logical
-#' @importFrom methods getPackageName
-#' @importFrom rlang env
 #' @export
 #'
 #' @examples
 #' check_that_compiled(fail_loudly = FALSE)
 check_that_compiled <- function(fail_loudly = TRUE){
-  is_compiled <- exists('read_edf_file',
-                        where=paste0('package:', getPackageName(env())),
-                        mode='function')
-
-  if (!is_compiled && !is.na(fail_loudly)) {
+  if (!eyelinkReader::is_compiled() && !is.na(fail_loudly)) {
     if (fail_loudly) {
       stop("No compiled interface to EDF API. Please see diagnostic messages when package is loading.")
     } else {
@@ -25,5 +18,20 @@ check_that_compiled <- function(fail_loudly = TRUE){
     }
   }
 
-  is_compiled
+  eyelinkReader::is_compiled()
+}
+
+
+#' Determine whether library is compiled and can be used
+#'
+#' @return logical
+#' @importFrom methods getPackageName
+#' @importFrom rlang env
+#' @export
+#' @examples
+#' is_compiled()
+is_compiled <- function(){
+  exists('read_edf_file',
+         where=paste0('package:', getPackageName(env())),
+         mode='function')
 }
