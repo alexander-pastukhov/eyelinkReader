f774ef2526052bee13f08f856446a456b084c1e8
Alexander Pastukhov <pastukhov.alexander@gmail.com>
2022-02-08 15:50:40 +0100

    Compilation with flags and hardcoded

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,12 @@
 .onLoad <- function(libname, pkgname) {
+  message("Compiling EDF API library interface, this will take moment...")
   temp_dir <- tempdir()
   file.copy(system.file("cpp", "edf_interface.cpp", package = pkgname), temp_dir)
   filename <- paste0(temp_dir, "/edf_interface.cpp")
-  # compiled_code <- Rcpp::sourceCpp(filename, env = parent.env(environment()))
+
+  # setting compilation flahs
+  Sys.setenv("PKG_CXXFLAGS"='-I"c:/Program Files (x86)/SR Research/EyeLink/Includes/eyelink"')
+  Sys.setenv("PKG_LIBS"='-L"c:/Program Files (x86)/SR Research/EyeLink/libs/x64" -ledfapi64')
+
+  Rcpp::sourceCpp(filename, env = parent.env(environment()))
 }
