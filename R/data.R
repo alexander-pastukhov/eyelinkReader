a12e1d130aaa8ef79c82b42a7e98a3ff70d08a06
Alexander Pastukhov <pastukhov.alexander@gmail.com>
2022-02-10 08:21:59 +0100

    Example imported object

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Imported example.edf, events only
+#'
+#' An \code{\link{eyelinkRecording}} object for _example.edf_,
+#' events only. See \code{\link{eyelinkRecording}} for details.
+"example"
+
+#' Imported example.edf, events and samples
+#'
+#' An \code{\link{eyelinkRecording}} object for _example.edf_,
+#' events and samples with all attributes.
+#' See \code{\link{eyelinkRecording}} for details.
+"example_with_samples"
