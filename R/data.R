16e7dcfe03caaedba005e1fcfcc254c3284d3b0e
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-18 12:11:33 +0100

    Brief description of the "experiment" behind recording

--- R/data.R
+++ R/data.R
@@ -2,7 +2,10 @@
 #'
 #' An \code{\link{eyelinkRecording}} for _example.edf_ via
 #' \code{read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"), import_samples = TRUE)}).
-#' Contains all extracted events including triggers, areas of interested, and display coordinates.
+#' Contains all extracted events including triggers, areas of interested, and display coordinates. The original
+#' recording consist of ten trials with a participant fixating on a target that jumps to a new location after
+#' one second and stays on the screen for another second. Includes all relevant events.
+#'
 #' See \code{\link{eyelinkRecording}} for details.
 #' @seealso \code{\link{eyelinkRecording}}, \link{read_edf}}
 "gaze"
