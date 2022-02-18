0430acccbc3535c4f8027467b8a791fbdcc5020e
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-02-18 12:09:43 +0100

    Extra \

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Imported example.edf, events and samples
 #'
-#' An \code{\link{eyelinkRecording}} for _example.edf_ via\
+#' An \code{\link{eyelinkRecording}} for _example.edf_ via
 #' \code{read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"), import_samples = TRUE)}).
 #' Contains all extracted events including triggers, areas of interested, and display coordinates.
 #' See \code{\link{eyelinkRecording}} for details.
