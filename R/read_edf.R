fcb0b58ab1ce7907ec462efd4272ca25facff2a1
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-07-22 11:40:09 +0200

    don't test example

--- R/read_edf.R
+++ R/read_edf.R
@@ -34,17 +34,17 @@
 #' @importFrom fs file_exists
 #' @importFrom dplyr %>% mutate mutate_if
 #' @examples
-#' if (eyelinkReader::is_compiled()) {
-#'     # Import only events and recordings information
-#'     recording <- read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"))
+#' \donttest{
+#'   # Import only events and recordings information
+#'    recording <- read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"))
 #'
-#'     # Import events and samples (only time and  screen gaze coordinates)
-#'     recording <- read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"),
-#'                       sample_attributes = c('time', 'gx', 'gy'))
+#'   # Import events and samples (only time and  screen gaze coordinates)
+#'   recording <- read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"),
+#'                         sample_attributes = c('time', 'gx', 'gy'))
 #'
-#'     # Import events and samples (all attributes)
-#'     recording <- read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"),
-#'                           import_samples= TRUE)
+#'   # Import events and samples (all attributes)
+#'   recording <- read_edf(system.file("extdata", "example.edf", package = "eyelinkReader"),
+#'                         import_samples= TRUE)
 #' }
 read_edf <- function(file,
                      consistency = 'check consistency and report',
