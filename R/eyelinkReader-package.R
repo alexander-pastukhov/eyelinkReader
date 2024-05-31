e0350e7d9aff6acdde0e5569af1057361528ad7c
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2024-05-31 12:47:59 +0200

    "_PACKAGE" as required by new rules

--- R/eyelinkReader-package.R
+++ R/eyelinkReader-package.R
@@ -1,16 +1,12 @@
-#' @keywords internal
-"_PACKAGE"
-
 ## usethis namespace: start
 #' @description Imports gaze data recorded by a SR Research EyeLink eye tracker from an EDF file.
 #' Includes options to import events and/or recorded samples and extract
 #' individual events such as saccades, fixations, blinks, and recorded variables.
 #'
-#' @docType package
 #' @import Rcpp
 #' @import RcppProgress
 #' @importFrom Rcpp evalCpp
 #' @useDynLib eyelinkReader
 #' @name eyelinkReader
 ## usethis namespace: end
-NULL
+"_PACKAGE"
